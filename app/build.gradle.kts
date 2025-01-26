bcf4c84bd13f4b7d039e611fa6ebcde0bac023e7
Seongdeok Hong <sdhong0609@gmail.com>
2025-01-26 14:58:24 +0900

    build: compileSdk 버전 업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hongstudio.flabrecyclerviewassignment"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hongstudio.flabrecyclerviewassignment"
