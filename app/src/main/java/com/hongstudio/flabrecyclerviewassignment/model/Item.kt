5df8a9f091f00b1b57095f02ba4579ba19c06e70
Seongdeok Hong <sdhong0609@gmail.com>
2025-02-09 16:09:04 +0900

    refactor: 두 개의 ListAdapter를 MainAdapter로 통합

--- app/src/main/java/com/hongstudio/flabrecyclerviewassignment/model/Item.kt
+++ app/src/main/java/com/hongstudio/flabrecyclerviewassignment/model/Item.kt
@@ -1,6 +1,16 @@
 package com.hongstudio.flabrecyclerviewassignment.model
 
-data class Item(
-    val id: Long,
+sealed interface Item {
+    val id: Long
     val title: String
-)
+
+    data class Normal(
+        override val id: Long,
+        override val title: String
+    ) : Item
+
+    data class Trash(
+        override val id: Long,
+        override val title: String
+    ) : Item
+}
